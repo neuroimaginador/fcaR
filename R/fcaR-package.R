d4f3ca7ab929a9c28258d5751606c3cabf1279c7
Domingo Lopez <dominlopez78@gmail.com>
2019-06-01 17:17:55 +0200

    Added Rcpp code

---
+++ R/fcaR-package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @useDynLib fcaR, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
---
+++ R/is_subset_sparse.R
@@ -0,0 +1,33 @@
+.is_subset_sparse <- function(x, y = NULL, proper = FALSE) {
+
+  if(is.null(y)) y <- x
+
+  if ("x" %in% slotNames(x)) {
+
+    my_x <- x@x
+
+  } else {
+
+    my_x <- rep(1, length(x@i))
+
+  }
+
+  if ("x" %in% slotNames(y)) {
+
+    my_y <- y@x
+
+  } else {
+
+    my_y <- rep(1, length(y@i))
+
+  }
+
+  p <- as.integer(rep(0, x@Dim[2]+1))
+  i <- is_subset_C(x@p, x@i, x@Dim, my_x,
+             y@p, y@i, y@Dim, my_y,
+             as.logical(proper), p)
+
+  t(new("ngCMatrix", p = p, i = i,
+        Dim = c(y@Dim[2], x@Dim[2])))
+
+}
