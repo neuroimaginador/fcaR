a24e47ba076b64cab38ef81658e5089ac3fd0c0d
Domingo Lopez <dominlopez78@gmail.com>
2019-06-20 20:00:18 +0200

    New function to filter the RHS by an attribute

--- R/implication_set.R
+++ R/implication_set.R
@@ -6,10 +6,15 @@ implication_set <- R6::R6Class(
   public = list(
 
     # Initialize with an optional name
-    initialize = function(name = "", attributes = c()) {
+    initialize = function(name = "",
+                          attributes = c(),
+                          lhs = NULL,
+                          rhs = NULL) {
 
       private$name <- name
       private$attributes <- attributes
+      private$lhs_matrix <- lhs
+      private$rhs_matrix <- rhs
 
     },
 
@@ -130,155 +135,22 @@ implication_set <- R6::R6Class(
 
     },
 
+    filter_by_rhs = function(attr_filter) {
 
-    compute_intersections = function() {
-
-      if (is.null(private$lhs_matrix)) {
-
-        message("Sparse matrices not previously computed. Computing now...")
-
-        self$compute_sparse_matrix()
-
-      }
-
-      logic_name <- tolower(fuzzy_logic()$name)
-
-      private$lhs_intersect_rhs <-
-        apply_F_elementwise(x = as.matrix(private$lhs_matrix),
-                            y = as.matrix(private$rhs_matrix),
-                            type = paste0(logic_name, "_T"))
-
-    },
-
-    batch_apply = function(rules = c("composition", "generalization"),
-                           batch_size = 25000) {
-
-      L <- .batch_apply(LHS = private$lhs_matrix,
-                        RHS = private$rhs_matrix,
-                        rules = rules,
-                        batch_size = batch_size)
-
-      private$lhs_matrix <- L$lhs
-      private$rhs_matrix <- L$rhs
-
-    },
-
-    apply_rules = function(rules) {
-
-      for (r in rules) {
-
-        private$implications <- .apply_rule(
-          implication_list = private$implications,
-          rule = r)
-
-      }
-
-    },
-
-
-    apply_r_simplification = function() {
-
-      if (is.null(private$lhs_subsets)) {
-
-        message("Subset matrices not previously computed. Computing now...")
-
-        self$compute_subsets()
-
-      }
-
-      if (is.null(private$lhs_intersect_rhs)) {
-
-        message("Intersection matrices not previously computed. Computing now...")
-
-        self$compute_intersections()
-
-        message("Computed.")
-
-      }
-
-      logic_name <- tolower(fuzzy_logic()$name)
-
-      LHS <- private$lhs_matrix
       RHS <- private$rhs_matrix
+      LHS <- private$lhs_matrix
 
-      new_LHS <- Matrix(0,
-                        nrow = length(private$attributes),
-                        ncol = 1,
-                        sparse = TRUE)
+      idx_attr <- match(attr_filter, private$attributes)
 
-      new_RHS <- Matrix(0,
-                        nrow = length(private$attributes),
-                        ncol = 1,
-                        sparse = TRUE)
+      idx <- which(RHS[idx_attr, ])
 
-      LHSURHS <- apply_F_elementwise(as.matrix(LHS),
-                                     as.matrix(RHS),
-                                     type = paste0(logic_name, "_S"))
-      LHSURHS <- Matrix(LHSURHS, sparse = TRUE)
+      imp <- implication_set$new(name = paste0(private$name, "_filter_", attr_filter),
+                                 attributes = private$attributes,
+                                 lhs = Matrix(LHS[, idx], sparse = TRUE),
+                                 rhs = Matrix(RHS[, idx], sparse = TRUE))
 
-      AinCD <- .is_subset_sparse(x = LHS, y = LHSURHS)
+      return(imp)
 
-      # This gives when A (from A -> B) is subset of CD (from C -> D)
-      condition1 <- rowSums(AinCD) > 1
-
-      # This gives those LHS which are disjoint to their RHS
-      condition2 <- colSums(private$lhs_intersect_rhs) == 0
-
-      are_subset <- which(condition1 & condition2)
-
-      marked_as_single <- rep(TRUE, ncol(LHS))
-
-      if (length(are_subset) > 0) {
-
-        for (subs in seq_along(are_subset)) {
-
-          this_row <- are_subset[subs]
-
-          my_idx <- which(AinCD[this_row, ])
-          my_idx <- setdiff(my_idx, this_row)
-          marked_as_single[my_idx] <- FALSE
-
-          # A <- as.matrix(LHS[, this_row])
-          B <- as.matrix(RHS[, this_row])
-
-          C <- as.matrix(LHS[, my_idx])
-          D <- as.matrix(RHS[, my_idx])
-
-          new_D <- apply_F_rowwise_xy(x = D,
-                                      y = B,
-                                      type = "set_diff")
-
-          L <- .compose_lhs_rhs_equal(Matrix(C, sparse = TRUE),
-                                      Matrix(new_D, sparse = TRUE))
-          C <- L$lhs
-          new_D <- L$rhs
-
-          L <- .remove_redundancies_lhs_rhs_general(C,
-                                                    new_D)
-          C <- L$lhs
-          new_D <- L$rhs
-
-          new_LHS <- cbind(new_LHS, C)
-          new_RHS <- cbind(new_RHS, new_D)
-
-        }
-
-      }
-
-      singles <- which(marked_as_single)
-
-      if (length(singles) > 0) {
-
-        new_LHS <- cbind(new_LHS, LHS[, singles])
-        new_RHS <- cbind(new_RHS, RHS[, singles])
-
-      }
-
-      private$lhs_matrix <- new_LHS[, -1]
-      private$rhs_matrix <- new_RHS[, -1]
-
-      private$lhs_subsets <- NULL
-      private$lhs_intersect_rhs <- NULL
 
     }
 
