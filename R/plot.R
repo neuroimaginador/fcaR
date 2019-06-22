11d33c25c7dffe79d54c3a7cbda41e36a36713b4
Domingo Lopez <dominlopez78@gmail.com>
2019-06-22 17:32:41 +0200

    Better organization of "to_string" methods

--- R/plot.R
+++ R/plot.R
@@ -1,36 +1,3 @@
-#' @import stringr
-.sparse_set_to_string <- function(S, attributes) {
-
-  idx <- which(S > 0)
-
-  if (length(idx) > 0) {
-
-    A <- S[idx]
-    att <- attributes[idx]
-
-    tmp <- paste0("{",
-           str_flatten(paste0(att, " [", A, "]"),
-                                collapse = ", "), "}")
-
-    gsub(pattern = "( \\[1\\])", replacement = "", x = tmp)
-
-  } else {
-
-    "{}"
-
-  }
-
-}
-
-.sparse_concept_to_string <- function(C, objects, attributes) {
-
-  A <- .sparse_set_to_string(C[[1]], objects)
-  B <- .sparse_set_to_string(C[[2]], attributes)
-
-  return(paste0("[", A, ", ", B, "]"))
-
-}
-
 #' @import hasseDiagram
 .draw_Hasse <- function(L, I) {
 
---
+++ R/to_string.R
@@ -0,0 +1,41 @@
+#' @import stringr
+.sparse_set_to_string <- function(S, attributes) {
+
+  idx <- which(S > 0)
+
+  if (length(idx) > 0) {
+
+    A <- S[idx]
+    att <- attributes[idx]
+
+    tmp <- paste0("{",
+                  str_flatten(paste0(att, " [", A, "]"),
+                              collapse = ", "), "}")
+
+    gsub(pattern = "( \\[1\\])", replacement = "", x = tmp)
+
+  } else {
+
+    "{}"
+
+  }
+
+}
+
+.sparse_concept_to_string <- function(C, objects, attributes) {
+
+  A <- .sparse_set_to_string(C[[1]], objects)
+  B <- .sparse_set_to_string(C[[2]], attributes)
+
+  return(paste0("[", A, ", ", B, "]"))
+
+}
+
+#' @export
+print_set <- function(A, attributes) {
+
+  A <- Matrix(A, sparse = TRUE)
+
+  .sparse_set_to_string(A, attributes)
+
+}
