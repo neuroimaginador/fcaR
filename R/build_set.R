ace70bd8a0a5e18175acdb253a2f7d400fe0b095
Domingo Lopez <dominlopez78@gmail.com>
2019-06-22 18:23:03 +0200

    Function to build a "sparse" set

---
+++ R/build_set.R
@@ -0,0 +1,12 @@
+#' @import Matrix
+#' @export
+build_set <- function(attrs, values, attributes) {
+
+  v <- rep(0, length(attributes))
+  names(v) <- attributes
+
+  v[attrs] <- values
+
+  Matrix(v, nrow = length(attributes), ncol = 1)
+
+}
