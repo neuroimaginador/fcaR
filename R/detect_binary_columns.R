4991a45fc4803ad0e5a33eaf761e47053179b2fc
Domingo Lopez <dominlopez78@gmail.com>
2019-07-04 13:05:39 +0200

    Better management of detection of binry columns

--- R/detect_binary_columns.R
+++ R/detect_binary_columns.R
@@ -1,6 +1,23 @@
 .detect_binary_columns <- function(I) {
 
-  sapply(seq(ncol(I)), function(col) all(sort(unique(I[, col])) == c(0, 1)))
+  sapply(seq(ncol(I)),
+
+         function(col) {
+
+           v <- sort(unique(I[, col]))
+
+           res <- length(v) == 2
+
+           if (res) {
+
+             res <- all(v == c(0, 1))
+
+           }
+
+           return(res)
+
+         }
+  )
 
 
 }
