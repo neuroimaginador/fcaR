c16c419d046fc9fe0cdff06bfe061a7db78d0e11
Domingo Lopez <dominlopez78@gmail.com>
2019-07-04 13:05:17 +0200

    Minor correction in function to add columns to sparse matrices

--- R/add_col_sparse.R
+++ R/add_col_sparse.R
@@ -1,11 +1,18 @@
 add_col <- function(M, sparse_v) {
 
+  sparse_v <- as(sparse_v, "dgCMatrix")
+
   newM <- M
 
   newM@i <- c(newM@i, as.integer(sparse_v@i))
   newM@p <- c(newM@p, max(newM@p) + length(sparse_v@i))
   newM@Dim[2] <- as.integer(newM@Dim[2] + 1)
-  newM@x <- c(newM@x, sparse_v@x)
+
+  if (!is(M, "ngCMatrix")) {
+
+    newM@x <- c(newM@x, sparse_v@x)
+
+  }
 
   return(newM)
 
@@ -18,7 +25,12 @@ add_cols <- function(M, cols) {
   newM@i <- c(newM@i, as.integer(cols@i))
   newM@p <- c(newM@p, max(newM@p) + cols@p[-1])
   newM@Dim[2] <- as.integer(newM@Dim[2] + cols@Dim[2])
-  newM@x <- c(newM@x, cols@x)
+
+  if (!is(M, "ngCMatrix")) {
+
+    newM@x <- c(newM@x, cols@x)
+
+  }
 
   return(newM)
 
