4deb4562b3df4e461a2e8a90cafd357745553855
Domingo Lopez <dominlopez78@gmail.com>
2019-10-15 21:07:03 +0200

    Added documentation to class implication set

--- R/implication_set.R
+++ R/implication_set.R
@@ -1,4 +1,8 @@
+#' @title
 #' R6 Class for Set of implications
+#'
+#' @description
+#' This class implements the structure needed to store implications and the methods associated.
 #'
 #' @import Matrix
 #' @export
@@ -17,7 +21,6 @@ implication_set <- R6::R6Class(
     #' @param rhs (a \code{dgCMatrix}) Optional initial RHS of the implications stored.
     #'
     #' @return A new \code{implication_set} object.
-    #'
     initialize = function(name = "",
                           attributes = c(),
                           lhs = NULL,
@@ -30,13 +33,28 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Get the names of the attributes
+    #'
+    #' @return A character vector with the names of the attributes used in the implications.
+    #'
+    #' @export
     get_attributes = function() {
 
       return(private$attributes)
 
     },
 
-    # Import from arules object
+    #' @description
+    #' Import from arules object
+    #'
+    #' @param arules_imp (\code{rules} object) The implications obtained with \code{arules} to be imported.
+    #'
+    #' @return Nothing, just updates the internal matrices for LHS and RHS.
+    #'
+    #' @import arules
+    #' @importFrom methods as is
+    #' @export
     from_arules = function(arules_imp) {
 
       attributes <- arules_imp@lhs@itemInfo$labels
@@ -53,6 +71,14 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Convert to arules format
+    #'
+    #' @return A \code{rules} object as used by package \code{arules}.
+    #'
+    #' @import arules
+    #' @importFrom methods as is
+    #' @export
     to_arules = function() {
 
       LHS <- as(private$lhs_matrix, "ngCMatrix")
@@ -69,7 +95,15 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Add new implication
+    #' @description
+    #' Add new implication to the set
+    #'
+    #' @param lhs  (a sparse matrix) The LHS of the rule to add.
+    #' @param rhs  (a sparse matrix) The RHS of the rule to add.
+    #'
+    #' @return Nothing, just updates the internal matrices for LHS and RHS.
+    #'
+    #' @export
     add_implication = function(lhs, rhs) {
 
       if (is.null(private$lhs_matrix)) {
@@ -97,6 +131,14 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Append implications to the current ones.
+    #'
+    #' @param implications (\code{ImplicationSet}) The set of implications to append at the end of the current set.
+    #'
+    #' @return Nothing, just updates the internal matrices for LHS and RHS.
+    #'
+    #' @export
     append_implications = function(implications) {
 
       LHS <- implications$get_LHS_matrix()
@@ -115,7 +157,12 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Number of implications in the set
+    #' @description
+    #' Cardinality: Number of implications in the set
+    #'
+    #' @return The cardinality of the implication set.
+    #'
+    #' @export
     cardinality = function() {
 
       if (self$is_empty()) return(0)
@@ -124,12 +171,24 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Empty set
+    #'
+    #' @return \code{TRUE} if the set of implications is empty, \code{FALSE} otherwise.
+    #'
+    #' @export
     is_empty = function() {
 
       is.null(private$lhs_matrix)
 
     },
 
+    #' @description
+    #' Size: number of attributes in each of LHS and RHS
+    #'
+    #' @return A vector with two components: the number of attributes present in each of the LHS and RHS of each implication in the set.
+    #'
+    #' @export
     size = function() {
 
       lhs_size <- colSums(private$lhs_matrix)
@@ -139,7 +198,16 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Compute the sintactic closure of a set wrt the implications
+    #' @description
+    #' Compute the semantic closure of a fuzzy set with respect to the implication set
+    #'
+    #' @param S        (a vector) Vector with the grades of each attribute (a fuzzy set).
+    #' @param reduce   (logical) Reduce the implications using simplification logic?
+    #' @param verbose  (logical) Show verbose output?
+    #'
+    #' @return If \code{reduce == FALSE}, the output is a fuzzy set corresponding to the closure of \code{S}. If \code{reduce == TRUE}, a list with two components: \code{closure}, with the closure as above, and \code{implications}, the reduced set of implications.
+    #'
+    #' @export
     compute_closure = function(S,
                                reduce = FALSE,
                                verbose = FALSE) {
@@ -166,6 +234,15 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Generate a recommendation for a subset of the attributes
+    #'
+    #' @param S        (a vector) Vector with the grades of each attribute (a fuzzy set).
+    #' @param attribute_filter (character vector) Names of the attributes to get recommendation for.
+    #'
+    #' @return A fuzzy set describing the values of the attributes in \code{attribute_filter} within the closure of \code{S}.
+    #'
+    #' @export
     recommend = function(S, attribute_filter) {
 
       .recommend_attribute(S = S,
@@ -176,6 +253,20 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Apply rules to remove redundancies
+    #'
+    #' @param rules       (character vector) Names of the rules to use. See \code{details}.
+    #' @param batch_size  (integer) If the number of rules is large, apply the rules by batches of this size.
+    #' @param parallelize (logical) If possible, should we parallelize the computation among different batches?
+    #' @param reorder     (logical) Should the rules be randonly reordered previous to the computation?
+    #'
+    #' @details
+    #' Currently, the implemented rules are \code{"generalization"}, \code{"simplification"}, \code{"reduction"} and \code{"composition"}.
+    #'
+    #' @return Nothing, just updates the internal matrices for LHS and RHS.
+    #'
+    #' @export
     apply_rules = function(rules = c("composition", "generalization"),
                            batch_size = 25000L,
                            parallelize = TRUE,
@@ -194,7 +285,12 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Print all implications to output
+    #' @description
+    #' Print all implications to text
+    #'
+    #' @return A string with all the implications in the set.
+    #'
+    #' @export
     print = function() {
 
       n_implications <- ncol(private$lhs_matrix)
@@ -210,6 +306,14 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Export to LaTeX
+    #'
+    #' @param ncols  (integer) Number of columns for the output.
+    #'
+    #' @return A string in LaTeX format that prints nicely all the implications.
+    #'
+    #' @export
     to_latex = function(ncols = 1) {
 
       imp_to_latex(self, ncols = ncols)
@@ -217,7 +321,12 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Get the sparse matrix for LHS
+    #' @description
+    #' Get internal LHS matrix
+    #'
+    #' @return A sparse matrix representing the LHS of the implications in the set.
+    #'
+    #' @export
     get_LHS_matrix = function() {
 
       if (self$is_empty()) {
@@ -237,7 +346,12 @@ implication_set <- R6::R6Class(
 
     },
 
-    # Get the sparse matrix for RHS
+    #' @description
+    #' Get internal RHS matrix
+    #'
+    #' @return A sparse matrix representing the RHS of the implications in the set.
+    #'
+    #' @export
     get_RHS_matrix = function() {
 
       if (self$is_empty()) {
@@ -257,6 +371,15 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Filter implications by attribute in RHS
+    #'
+    #' @param attr_filter  (character vector) Names of the attributes to filter by.
+    #' @param drop         (logical) Remove the rest of attributes in RHS?
+    #'
+    #' @return An \code{ImplicationSet} that is a subset of the current set, only with those rules which has the attributes in \code{attribute_filter} in their RHS.
+    #'
+    #' @export
     filter_by_rhs = function(attr_filter, drop = FALSE) {
 
       RHS <- private$rhs_matrix
@@ -305,9 +428,16 @@ implication_set <- R6::R6Class(
       warning("No RHS with that attribute, sorry.")
       return(invisible(NULL))
 
-
     },
 
+    #' @description
+    #' Filter implications by attribute in LHS
+    #'
+    #' @param attr_filter  (character vector) Names of the attributes to filter by.
+    #'
+    #' @return An \code{ImplicationSet} that is a subset of the current set, only with those rules which has the attributes in \code{attribute_filter} in their LHS.
+    #'
+    #' @export
     filter_by_lhs = function(attr_filter) {
 
       RHS <- private$rhs_matrix
@@ -340,9 +470,16 @@ implication_set <- R6::R6Class(
       warning("No LHS with that attribute, sorry.")
       return(invisible(NULL))
 
-
     },
 
+    #' @description
+    #' Get a subset of the implication set
+    #'
+    #' @param idx   (integer vector) Indices of the implications to extract.
+    #'
+    #' @return A new \code{ImplicationSet} with only the rules given by the \code{idx} indices.
+    #'
+    #' @export
     get_rules = function(idx) {
 
       RHS <- private$rhs_matrix
@@ -357,6 +494,14 @@ implication_set <- R6::R6Class(
 
     },
 
+    #' @description
+    #' Remove implications from the set
+    #'
+    #' @param idx  (integer vector) Indices of implications to remove.
+    #'
+    #' @return An \code{ImplicationSet} with the implications at given indices removed with respect to the original set.
+    #'
+    #' @export
     remove_rules = function(idx) {
 
       idx <- idx[idx < ncol(private$lhs_matrix)]
