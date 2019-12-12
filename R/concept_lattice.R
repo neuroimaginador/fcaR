a5296f509e7cf1b1f11bec7fcde353b953399545
Domingo Lopez <dominlopez78@gmail.com>
2019-12-12 16:59:28 +0100

    Initial version for ConceptLattice class

---
+++ R/concept_lattice.R
@@ -0,0 +1,32 @@
+ConceptLattice <- R6::R6Class(
+
+  classname = "ConceptLattice",
+
+  public = list(
+
+    initialize = function() {
+
+
+    },
+
+    plot = function() {
+
+
+    },
+
+    print = function() {
+
+
+    }
+
+  ),
+
+  private = list(
+
+    concepts = NULL,
+    objects = NULL,
+    attributes = NULL,
+
+  )
+
+)
