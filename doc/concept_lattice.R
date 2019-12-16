13dc45020de82a660a62866a6b593b1d41c8dfbc
Domingo Lopez <dominlopez78@gmail.com>
2019-12-16 10:48:50 +0100

    Added vignette in doc/ folder

---
+++ doc/concept_lattice.R
@@ -0,0 +1,188 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.width = 7,
+  fig.height = 5
+)
+
+## ----setup--------------------------------------------------------------------
+library(fcaR)
+
+## ----echo = FALSE-------------------------------------------------------------
+objects <- c("Mercury", "Venus", "Earth", "Mars",
+             "Jupiter", "Saturn", "Uranus", "Neptune",
+             "Pluto")
+
+attributes <- c("small", "medium", "large",
+                "near", "far",
+                "moon", "no_moon")
+
+planets <- matrix(0, nrow = length(objects),
+                  ncol = length(attributes))
+
+rownames(planets) <- objects
+colnames(planets) <- attributes
+
+planets["Mercury", c("small", "near", "no_moon")] <- 1
+planets["Venus", c("small", "near", "no_moon")] <- 1
+planets["Earth", c("small", "near", "moon")] <- 1
+planets["Mars", c("small", "near", "moon")] <- 1
+planets["Jupiter", c("large", "far", "moon")] <- 1
+planets["Saturn", c("large", "far", "moon")] <- 1
+planets["Uranus", c("medium", "far", "moon")] <- 1
+planets["Neptune", c("medium", "far", "moon")] <- 1
+planets["Pluto", c("small", "far", "moon")] <- 1
+
+## -----------------------------------------------------------------------------
+knitr::kable(planets, format = "html", booktabs = TRUE)
+
+## ----echo = FALSE-------------------------------------------------------------
+objects <- paste0("O", 1:6)
+n_objects <- length(objects)
+
+attributes <- paste0("P", 1:6)
+n_attributes <- length(attributes)
+
+I <- matrix(data = c(0, 1, 0.5, 0, 0, 0.5,
+                     0, 1, 0.5, 0, 0, 0.5,
+                     0.5, 1, 0, 0, 1, 0,
+                     0.5, 0, 0, 1, 0.5, 0,
+                     1, 0, 0, 0.5, 0, 0,
+                     0, 0, 1, 0, 0, 1),
+            nrow = n_objects,
+            byrow = FALSE)
+
+colnames(I) <- attributes
+rownames(I) <- objects
+
+## -----------------------------------------------------------------------------
+knitr::kable(I, format = "html", booktabs = TRUE)
+
+## -----------------------------------------------------------------------------
+fc_planets <- FormalContext$new(planets)
+fc_I <- FormalContext$new(I)
+
+## -----------------------------------------------------------------------------
+print(fc_planets)
+print(fc_I)
+
+## ----fig.dim=c(4,4)-----------------------------------------------------------
+fc_planets$plot()
+fc_I$plot()
+
+## -----------------------------------------------------------------------------
+fc_planets$to_latex()
+
+## -----------------------------------------------------------------------------
+# Define a set of objects
+S <- SparseSet$new(attributes = fc_planets$objects)
+S$assign(attributes = c("Earth", "Mars"), values = c(1, 1))
+S
+
+# Compute the intent of S
+fc_planets$intent(S)
+
+## -----------------------------------------------------------------------------
+# Define a set of objects
+S <- SparseSet$new(attributes = fc_planets$attributes)
+S$assign(attributes = c("moon", "large"), values = c(1, 1))
+S
+
+# Compute the extent of S
+fc_planets$extent(S)
+
+## -----------------------------------------------------------------------------
+# Compute the closure of S
+Sc <- fc_planets$closure(S)
+Sc
+
+## -----------------------------------------------------------------------------
+fc_planets$is_closed(S)
+fc_planets$is_closed(Sc)
+
+## -----------------------------------------------------------------------------
+fc_planets$reduce(TRUE)
+
+fc_I$clarify(TRUE)
+
+## -----------------------------------------------------------------------------
+fc_planets$find_implications()
+
+fc_I$find_implications()
+
+## -----------------------------------------------------------------------------
+# Concepts
+fc_planets$concepts
+
+# Implications
+fc_planets$implications
+
+## -----------------------------------------------------------------------------
+fc_planets$standardize()
+fc_I$standardize()
+
+## ----eval = FALSE-------------------------------------------------------------
+#  fc$save(filename = "./fc.rds")
+#  
+#  # Create an empty FormalContext where to
+#  # import the previously saved
+#  fc2 <- FormalContext$new()
+#  fc2$load("./fc.rds")
+
+## -----------------------------------------------------------------------------
+fc_planets$concepts$plot()
+fc_I$concepts$plot()
+
+## -----------------------------------------------------------------------------
+# Printing
+fc_planets$concepts
+
+# LaTeX
+fc_planets$concepts$to_latex()
+
+## -----------------------------------------------------------------------------
+fc_planets$concepts[2:3]
+
+## ----eval = FALSE-------------------------------------------------------------
+#  fc_planets$concepts$extents()
+#  fc_planets$concepts$intents()
+
+## -----------------------------------------------------------------------------
+fc_planets$concepts$support()
+
+## -----------------------------------------------------------------------------
+# Get the index of those concepts with support 
+# greater than the threshold
+idx <- which(fc_I$concepts$support() > 0.2)
+# Build the sublattice
+sublattice <- fc_I$concepts$sublattice(idx)
+sublattice
+
+
+## -----------------------------------------------------------------------------
+sublattice$plot()
+
+## -----------------------------------------------------------------------------
+# The fifth concept
+C <- fc_planets$concepts[5][[1]]
+C
+# Its subconcepts:
+fc_planets$concepts$subconcepts(C)
+# And its superconcepts:
+fc_planets$concepts$superconcepts(C)
+
+## -----------------------------------------------------------------------------
+# A list of concepts
+C <- fc_planets$concepts[5:7]
+C
+
+# Supremum of the concepts in C
+fc_planets$concepts$supremum(C)
+# Infimum of the concepts in C
+fc_planets$concepts$infimum(C)
+
+## -----------------------------------------------------------------------------
+fc_planets$concepts$join_irreducibles()
+fc_planets$concepts$meet_irreducibles()
+
