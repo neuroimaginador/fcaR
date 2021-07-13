330b2cf69d610a9eddd8caf78315b4fa3f23d3e0
amorabonilla <amorabonilla.github.io>
2021-07-13 18:20:35 +0200

    compute size

---
+++ R/tamaño.R
@@ -0,0 +1,19 @@
+#' Tamaño
+#'
+#' @param implications  This is the set of implications
+#'
+#' @return
+#' The size of the set of implications
+#' @export
+#'
+#' @examples
+#' library(fcaR)
+#' fc <- FormalContext$new(planets)
+#' fc$find_implications()
+#' mitotal <- tamaño(fc$implications)
+tamaño <- function(implications){
+  # calcula el tamaño de un conjunto de implicaciones
+  total <- sum(implications$size())
+  return(total)
+}
+
